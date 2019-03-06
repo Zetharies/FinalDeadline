bb456fa818566fef4d7eafc9b50ea24f7914c8ec
kunall-ll <kunal.agarwala6@gmail.com>
2019-03-06 11:49:29 +0000

    Add files via upload

---
+++ core/assets/maps/Optometry/Tilesets/Carpet.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Carpet" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../Images/Carpet.png" width="96" height="96"/>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="32" height="32.75"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ core/assets/maps/Optometry/Tilesets/Open-Tileset.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Open-Tileset" tilewidth="32" tileheight="32" tilecount="616" columns="22">
+ <image source="../Images/Open-Tileset.png" width="708" height="908"/>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="7" x="0" y="0" width="32" height="31.75"/>
+   <object id="8" x="0.181818" y="0" width="31.4545" height="31.4545"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="0.181818" y="0" width="32" height="32.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="199">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.181818" width="31.8182" height="31.8182"/>
+  </objectgroup>
+ </tile>
+ <tile id="221">
+  <objectgroup draworder="index">
+   <object id="1" x="7.09091" y="-1.27273" width="18.3636" height="15.2727"/>
+  </objectgroup>
+ </tile>
+</tileset>
