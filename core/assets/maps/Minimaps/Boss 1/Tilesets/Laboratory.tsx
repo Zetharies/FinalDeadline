856a613b4befbfdeb5a8e668f69865f5f5109676
kunall-ll <kunal.agarwala6@gmail.com>
2019-03-12 15:13:48 +0000

    Add files via upload

---
+++ core/assets/maps/Minimaps/Boss 1/Tilesets/Laboratory.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Laboratory" tilewidth="32" tileheight="32" tilecount="315" columns="35">
+ <image source="../Images/Laboratory.png" width="1136" height="304"/>
+ <tile id="195">
+  <objectgroup draworder="index">
+   <object id="1" x="6.5" y="4.75" width="26" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="196">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="5.5" width="32.5" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="197">
+  <objectgroup draworder="index">
+   <object id="1" x="7" y="4.75" width="25.5" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="198">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="5.5" width="31.5" height="26.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="199">
+  <objectgroup draworder="index">
+   <object id="1" x="6.5" y="4.5" width="26.25" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="200">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.75" y="5.5" width="33.25" height="26.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index">
+   <object id="1" x="8" y="5.25" width="24.75" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="202">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="4.5" width="32.25" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="230">
+  <objectgroup draworder="index">
+   <object id="1" x="5" y="-0.75" width="27.25" height="19"/>
+   <object id="2" x="1" y="27.25" width="32.25" height="4.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="237">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="18.5"/>
+   <object id="2" x="0.25" y="27" width="32" height="4.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="265">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.75" y="-0.5" width="33.75" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="272">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.75" width="31.75" height="31.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="300">
+  <objectgroup draworder="index">
+   <object id="1" x="1.25" y="0.5" width="30.25" height="27.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="301">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.75" width="32" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="302">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.75" width="32" height="27.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="303">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="32.25" height="27.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="304">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="0.25" width="32.75" height="28.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="305">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.5" width="32" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="306">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.5" width="32.5" height="27.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="307">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="31.25" height="28"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ core/assets/maps/Minimaps/Boss 1/Tilesets/Open-Tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="Open-Tileset" tilewidth="32" tileheight="32" tilecount="616" columns="22">
+ <image source="../Images/Open-Tileset.png" width="708" height="908"/>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0" width="32.25" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
