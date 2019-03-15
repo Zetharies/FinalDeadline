086dec5bf837a9dd8236fbe90c2789e4cac20283
Bhaven Patel <bhavenpatel19@hotmail.co.uk>
2019-03-15 16:48:58 +0000

    Added Vents to Biology Map

--- core/assets/maps/floor4/Tile Sets/Open-Tileset.tsx
+++ core/assets/maps/floor4/Tile Sets/Open-Tileset.tsx
@@ -1,24 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="Open-Tileset" tilewidth="32" tileheight="32" tilecount="616" columns="22">
  <image source="../Images/Open-Tileset.png" width="708" height="908"/>
- <tile id="16">
-  <properties>
-   <property name="invisible" value="true"/>
-  </properties>
- </tile>
  <tile id="18">
   <properties>
-   <property name="blocked" value="true"/>
-  </properties>
- </tile>
- <tile id="20">
-  <properties>
-   <property name="invisible" value="true"/>
-  </properties>
- </tile>
- <tile id="33">
-  <properties>
-   <property name="invisible" value="true"/>
+   <property name="Blocked" value="true"/>
   </properties>
  </tile>
 </tileset>
